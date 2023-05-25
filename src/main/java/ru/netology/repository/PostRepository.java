03d9ca8d269d9d894efb714739eb5b3bd2d1e53b
artmysen <artmysen@gmail.com>
2023-05-25 20:13:39 +0300

    Initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,48 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+  AtomicLong countId;
+  ConcurrentHashMap<Long, String> postsMap;
+  public PostRepository() {
+    postsMap = new ConcurrentHashMap<Long, String>();
+    countId = new AtomicLong(1);
+  }
+  public ConcurrentHashMap all() {
+    return postsMap;
+  }
+  public Optional<Post> getById(long id) {
+    return Optional.of(new Post(id, postsMap.get(id)));
+  }
+  public Post save(Post post) {
+
+    long valId = post.getId();
+
+    if (valId != 0) {
+      if (valId > countId.get() + 1) {
+        valId = countId.get();
+        countId.incrementAndGet();
+      }
+      postsMap.put(valId, post.getContent());
+      return post;
+    } else {
+      postsMap.put(countId.get(), post.getContent());
+      countId.incrementAndGet();
+    }
+    return post;
+  }
+  public void removeById(long id) {
+    if (!postsMap.containsKey(id)) {
+      return;
+    }
+    postsMap.remove(id);
+  }
+}
---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,29 @@
+package ru.netology.service;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class  PostService {
+  private final PostRepository repository;
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+  public ConcurrentHashMap all() {
+    return (ConcurrentHashMap) repository.all();
+  }
+  public Post getById(long id) {
+    return repository.getById(id).orElseThrow(NotFoundException::new);
+  }
+
+  public Post save(Post post) {
+    return repository.save(post);
+  }
+
+  public void removeById(long id) {
+    repository.removeById(id);
+  }
+}
+
